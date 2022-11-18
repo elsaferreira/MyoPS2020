5cdabca8dce294616fa35160f85a59258eb03a66
Elsa Ferreira <112973327+elsaferreira@users.noreply.github.com>
2022-11-18 09:56:39 +0000

    Add My_path

--- path_config.py
+++ path_config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 path_dict = {}
 path_dict['MyoPS_data_dir'] = "/home/datasets/heart/MyoPS"
+path_dict['My_path'] = "/home/elsaferreira/local/src"
 path_dict['nnunet_raw_data_dir'] = "/home/datasets/heart/MyoPS/nnUNet_raw_data_base/nnUNet_raw_data"
 
